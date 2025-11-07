8318fd7b190745d4ceb52262b0d488b2eff4a654
Dada Johnson <iam.jdada@gmail.com>
2025-11-07 01:50:29 +0100

    login redirect

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('admin.dashboard', absolute: false));
+        return redirect(route('admin.dashboard', absolute: false));
     }
 
     /**
